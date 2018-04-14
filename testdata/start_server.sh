da5d5421f7bf3ca3367ed9593df927b27e29a381
Chyroc <chen_yunpeng@foxmail.com>
2018-04-15 00:17:50 +0800

    fix

--- testdata/start_server.sh
+++ testdata/start_server.sh
@@ -2,10 +2,20 @@
 
 set -e
 
-docker run --name mysql_db_server -e MYSQL_ROOT_PASSWORD=pass -d mysql
+function retry_curl {
+while ! curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 10 'http://127.0.0.1:'$1
+do
+    { echo "Exit status of curl: $?"
+      echo "Retrying ..."
+    } 1>&2
+    sleep 1
+done
+}
 
-sleep 5
+docker run --name mysql_db_server -e MYSQL_ROOT_PASSWORD=pass -d -p 3306:3306 mysql
+
+retry_curl 3306
 
 docker run --name myadmin -d --link mysql_db_server:db -p 8000:80 phpmyadmin/phpmyadmin
 
-sleep 5
+retry_curl 8000
