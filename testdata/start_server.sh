393316c6c5376104174f9dde8b2da8024791beb3
Chyroc <chen_yunpeng@foxmail.com>
2018-04-15 00:03:42 +0800

    fix

--- testdata/start_server.sh
+++ testdata/start_server.sh
@@ -4,4 +4,8 @@ set -e
 
 docker run --name mysql_db_server -e MYSQL_ROOT_PASSWORD=pass -d mysql
 
+sleep 5
+
 docker run --name myadmin -d --link mysql_db_server:db -p 8000:80 phpmyadmin/phpmyadmin
+
+sleep 5
