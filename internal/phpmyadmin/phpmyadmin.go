1f2b82d8d49e219979ab1f1b6158ce26b9be88c9
Chyroc <chen_yunpeng@foxmail.com>
2018-04-15 19:59:38 +0800

    refactor request-and-refresh-token

---
+++ internal/phpmyadmin/phpmyadmin.go
@@ -0,0 +1,156 @@
+package phpmyadmin
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+
+	"github.com/Chyroc/phpmyadmin-cli/internal/common"
+	"github.com/Chyroc/phpmyadmin-cli/internal/requests"
+	"github.com/Chyroc/phpmyadmin-cli/internal/utils"
+)
+
+var DefaultPHPMyAdmin *phpMyAdmin
+var tokenRegexp = regexp.MustCompile("<input type=\"hidden\" name=\"token\" value=\"(.*?)\" [/]>")
+
+type phpMyAdmin struct {
+	session *requests.Session
+	Token   string
+	uri     string
+}
+
+type phpMyAdminResp struct {
+	Message string
+	Success bool
+	Error   string
+}
+
+type Server struct {
+	ID   string
+	Name string
+}
+
+type Servers struct {
+	S []Server
+}
+
+func (s *Servers) Print() {
+	for _, v := range s.S {
+		common.Info(fmt.Sprintf("%s: %s\n", v.ID, v.Name))
+	}
+}
+func init() {
+	DefaultPHPMyAdmin = &phpMyAdmin{
+		session: requests.DefaultSession,
+	}
+}
+
+func (p *phpMyAdmin) SetURI(uri string) {
+	p.uri = uri
+}
+
+func (p *phpMyAdmin) initCookie() error {
+	_, err := p.Get(p.uri, "index.php", nil)
+	return err
+}
+
+func (p *phpMyAdmin) Login(username, password string) (err error) {
+	defer func() {
+		if err != nil {
+			common.Error(err)
+		}
+	}()
+
+	if err = p.initCookie(); err != nil {
+		return err
+	}
+	body := fmt.Sprintf("pma_username=%s&pma_password=%s&token=%s", username, password, utils.Escape(p.Token))
+	header := map[string]string{"Content-Type": "application/x-www-form-urlencoded"}
+
+	if _, err := p.Post(p.uri, "index.php", nil, header, strings.NewReader(body)); err != nil {
+		return err
+	}
+
+	result, err := p.Get(p.uri, "server_status_processes.php", nil)
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(string(result), "SHOW PROCESSLIST") {
+		return fmt.Errorf("login err")
+	}
+
+	common.Info("login as [%s] success\n", username)
+	return nil
+}
+
+func (p *phpMyAdmin) GetServerList(url string) (*Servers, error) {
+	b, err := p.Get(url, "", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if common.IsDebug1 {
+		common.Debug("return %s\n", string(b))
+	}
+
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+
+	var s []Server
+	doc.Find("#select_server > option").Each(func(_ int, selection *goquery.Selection) {
+		id := strings.TrimSpace(selection.AttrOr("value", ""))
+		name := strings.TrimSpace(selection.Text())
+
+		if id != "" {
+			s = append(s, Server{id, name})
+		}
+	})
+
+	return &Servers{s}, nil
+}
+
+func (p *phpMyAdmin) ExecSQL(server, database, table, sql string) ([]byte, error) {
+	if p.Token == "" {
+		p.initCookie()
+	}
+
+	data := map[string]string{
+		// "table":             table,
+		// "prev_sql_query":    "",
+		"db":                database,
+		"server":            server,
+		"token":             p.Token,
+		"sql_query":         sql,
+		"ajax_request":      "true",
+		"ajax_page_request": "true",
+	}
+	var bs []string
+	for k, v := range data {
+		bs = append(bs, k+"="+utils.Escape(v))
+	}
+	body := strings.NewReader(strings.Join(bs, "&"))
+	common.Debug("ExecSQL body [%v]\n", strings.Join(bs, "&"))
+	header := map[string]string{"Content-Type": "application/x-www-form-urlencoded"}
+
+	b, err := p.Post(p.uri+"/import.php", "", nil, header, body)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("result %s\n", b)
+
+	var r phpMyAdminResp
+	if err = json.Unmarshal(b, &r); err != nil {
+		return nil, err
+	}
+	common.Debug("ExecSQL [%v]:[%v]:[%v]\n", r.Success, r.Error, r.Message)
+
+	return handlerPhpmyadminResp(r)
+}
--- internal/phpmyadmin/request_test.go
+++ internal/phpmyadmin/request_test.go
@@ -1,9 +1,13 @@
 package phpmyadmin
 
 import (
+	"bytes"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/Chyroc/phpmyadmin-cli/internal/requests"
+	"github.com/Chyroc/phpmyadmin-cli/internal/show"
 )
 
 func initClient() *phpMyAdmin {
@@ -12,19 +16,122 @@ func initClient() *phpMyAdmin {
 		panic(err)
 	}
 	return &phpMyAdmin{
-		Session: s,
+		session: s,
 	}
 }
 
 func TestLogin(t *testing.T) {
+	as := assert.New(t)
 	p := initClient()
 	p.SetURI("localhost:8000")
 
-	if err := p.Login("root", "pass"); err != nil {
-		t.Errorf(err.Error())
-	}
+	as.Nil(p.Login("root", "pass"))
+	as.NotNil(p.Login("root", "error"))
+}
 
-	if err := p.Login("root", "error"); err == nil {
-		t.Errorf("should return err")
-	}
+func TestShowDatabases(t *testing.T) {
+	p := initClient()
+	as := assert.New(t)
+	p.SetURI("localhost:8000")
+
+	as.Nil(p.Login("root", "pass"))
+
+	b, err := p.ExecSQL("root", "", "", "show databases;")
+	as.Nil(err)
+	as.NotNil(b)
+
+	var buf = new(bytes.Buffer)
+	show.TestSetOut(buf)
+	show.ParseFromHTML(string(b))
+	as.Equal(`+--------------------+
+|      Database      |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+`, buf.String())
+}
+
+func TestShowTable(t *testing.T) {
+	p := initClient()
+	as := assert.New(t)
+	p.SetURI("localhost:8000")
+
+	as.Nil(p.Login("root", "pass"))
+
+	b, err := p.ExecSQL("root", "information_schema", "", "show tables;")
+	as.Nil(err)
+	as.NotNil(b)
+
+	var buf = new(bytes.Buffer)
+	show.TestSetOut(buf)
+	show.ParseFromHTML(string(b))
+	as.Equal(`+---------------------------------------+
+|     Tables_in_information_schema      |
++---------------------------------------+
+| CHARACTER_SETS                        |
+| COLLATIONS                            |
+| COLLATION_CHARACTER_SET_APPLICABILITY |
+| COLUMNS                               |
+| COLUMN_PRIVILEGES                     |
+| ENGINES                               |
+| EVENTS                                |
+| FILES                                 |
+| GLOBAL_STATUS                         |
+| GLOBAL_VARIABLES                      |
+| KEY_COLUMN_USAGE                      |
+| OPTIMIZER_TRACE                       |
+| PARAMETERS                            |
+| PARTITIONS                            |
+| PLUGINS                               |
+| PROCESSLIST                           |
+| PROFILING                             |
+| REFERENTIAL_CONSTRAINTS               |
+| ROUTINES                              |
+| SCHEMATA                              |
+| SCHEMA_PRIVILEGES                     |
+| SESSION_STATUS                        |
+| SESSION_VARIABLES                     |
+| STATISTICS                            |
+| TABLES                                |
+| TABLESPACES                           |
+| TABLE_CONSTRAINTS                     |
+| TABLE_PRIVILEGES                      |
+| TRIGGERS                              |
+| USER_PRIVILEGES                       |
+| VIEWS                                 |
+| INNODB_LOCKS                          |
+| INNODB_TRX                            |
+| INNODB_SYS_DATAFILES                  |
+| INNODB_FT_CONFIG                      |
+| INNODB_SYS_VIRTUAL                    |
+| INNODB_CMP                            |
+| INNODB_FT_BEING_DELETED               |
+| INNODB_CMP_RESET                      |
+| INNODB_CMP_PER_INDEX                  |
+| INNODB_CMPMEM_RESET                   |
+| INNODB_FT_DELETED                     |
+| INNODB_BUFFER_PAGE_LRU                |
+| INNODB_LOCK_WAITS                     |
+| INNODB_TEMP_TABLE_INFO                |
+| INNODB_SYS_INDEXES                    |
+| INNODB_SYS_TABLES                     |
+| INNODB_SYS_FIELDS                     |
+| INNODB_CMP_PER_INDEX_RESET            |
+| INNODB_BUFFER_PAGE                    |
+| INNODB_FT_DEFAULT_STOPWORD            |
+| INNODB_FT_INDEX_TABLE                 |
+| INNODB_FT_INDEX_CACHE                 |
+| INNODB_SYS_TABLESPACES                |
+| INNODB_METRICS                        |
+| INNODB_SYS_FOREIGN_COLS               |
+| INNODB_CMPMEM                         |
+| INNODB_BUFFER_POOL_STATS              |
+| INNODB_SYS_COLUMNS                    |
+| INNODB_SYS_FOREIGN                    |
+| INNODB_SYS_TABLESTATS                 |
++---------------------------------------+
+`, buf.String())
 }
